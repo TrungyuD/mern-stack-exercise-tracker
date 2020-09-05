11065681a3c687c01c22a561107812c1b920914c
TrungyuD <shawnkool10@gmail.com>
2020-09-05 18:19:19 +0700

    update

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('build'));
+app.use(express.static('client/build'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname,'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname,'client','build', 'index.html'));
 });
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology:true,
