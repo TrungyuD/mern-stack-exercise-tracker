66c7115cfe7c8e080facd7357f23f5cfedc0738a
TrungyuD <shawnkool10@gmail.com>
2020-09-05 17:50:15 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const path = require('path');
 require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'build', 'index.html'));
+});
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
     useNewUrlParser: true,
